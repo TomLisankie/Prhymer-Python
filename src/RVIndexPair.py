ede517e74817e0cd58ec97169ad74b6c17e82f4e
Thomas Lisankie <link285@gmail.com>
2016-12-15 00:56:44 -0500

    -

--- src/RVIndexPair.py
+++ src/RVIndexPair.py
@@ -16,6 +16,9 @@ class RVIndexPair(object):
     def addIndexes(self, indexesToAdd, RVBetweenPhonemes):
         for index in indexesToAdd:
             self.indexes.append(index)
+            '''Indexes that are being compared for rhyme value
+            differ between Java and Python versions for some
+            reason.'''
         self.rhymeValueForSet = self.rhymeValueForSet + RVBetweenPhonemes
         
     def attachChildNode(self, childNode):
